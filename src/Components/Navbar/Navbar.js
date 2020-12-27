268afa943eea817065828a4b71955264befb3eab
vasileBrendea <vasybrendea@gmail.com>
2020-12-27 16:53:53 +0200

    start style navbar (Home-page)

---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from "../SVG/logo.svg";
+import styles from "./Navbar.module.scss";
+
+const Navbar = () => {
+    return (
+        <div className={styles.navbar}>
+            <div className={styles.logo}>
+                <a href="/"><img src={logo} alt=""/></a>
+            </div>
+            <div className={styles.buttons}>
+                <button className={styles.about}>About</button>
+                <button className={styles.contact}>Contact</button>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
