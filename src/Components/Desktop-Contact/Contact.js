46461cb98adc79fa0eaea66d5ef3e4b6f5500e67
vasileBrendea <vasybrendea@gmail.com>
2020-12-30 13:31:08 +0200

    reorganize navbar buttons

--- src/Components/Desktop-Contact/Contact.js
+++ src/Components/Desktop-Contact/Contact.js
@@ -1,40 +1,40 @@
 import React from "react";
 import Navbar from "../Navbar/Navbar";
-import styles from './Contact.module.scss';
+import styles from "./Contact.module.scss";
 import contactImage from "../SVG/contactImage.svg";
 import Vector3 from "../SVG/Vector3.svg";
 import Input from "../Input/Input";
 
 const Contact = () => {
-    return (
-        <div >
-            <Navbar />
-            <div className={styles.contactPage}>
-                  <div className={styles.contactInput}>
-                        <div className={styles.contactHeader}>
-                            <h1 className={styles.contactH1}>Contact</h1>
-                        </div>
-                        <div>
-                            <Input label="Name" type="string"></Input>
-                            <Input label="Email" type="email"></Input>
-                            <div className={styles.text}>
-                                <p className={styles.inputP}>Text</p>
-                                <input className={styles.inputField2} type="string" name="User-name"></input>
-                            </div>
-                        </div>
-                        <div className={styles.button}>
-                            <button className={styles.sendButton}>Send</button>
-                        </div>
-                  </div>
-                  <div className={styles.contactImage}>
-                        <img src={contactImage} alt="" />
-                  </div>
+  return (
+    <div>
+      <Navbar />
+      <div className={styles.contactPage}>
+        <div className={styles.contactInput}>
+          <div className={styles.contactHeader}>
+            <h1 className={styles.contactH1}>Contact</h1>
+          </div>
+          <div>
+            <Input label="Name" type="string"></Input>
+            <Input label="Email" type="email"></Input>
+            <div className={styles.text}>
+              <p className={styles.inputP}>Text</p>
+              <textarea className={styles.inputField2} type="string" name="User-name"></textarea>
             </div>
-            <div className={styles.vector3Image}>
-                    <img src={Vector3} alt="" />
-            </div>
+          </div>
+          <div className={styles.button}>
+            <button className={styles.sendButton}>Send</button>
+          </div>
         </div>
-    )
-}
+        <div className={styles.contactImage}>
+          <img src={contactImage} alt="" />
+        </div>
+      </div>
+      <div className={styles.vector3Image}>
+        <img src={Vector3} alt="" />
+      </div>
+    </div>
+  );
+};
 
-export default Contact
+export default Contact;
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,21 +1,28 @@
-import React from 'react';
+import React from "react";
 import logo from "../SVG/logo.svg";
 import styles from "./Navbar.module.scss";
 
-import {NavLink} from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
-    return (
-        <div className={styles.navbar}>
-            <NavLink to="/" exact className={styles.logo} activeClassName={styles.active} >
-                <a href="/"><img src={logo} alt=""/></a>
-            </NavLink>
-            <div className={styles.buttons}>
-                <NavLink to="/about" className={styles.button} activeClassName={styles.active}>About</NavLink>
-                <NavLink to="/contact" className={styles.button} activeClassName={styles.active}>Contact</NavLink>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className={styles.navbar}>
+      <NavLink to="/" exact className={styles.logo} activeClassName={styles.active}>
+        <a href="/">
+          <img src={logo} alt="" />
+        </a>
+      </NavLink>
+      <div className={styles.buttons}>
+        <NavLink to="/about" className={styles.button} activeClassName={styles.active}>
+          About
+        </NavLink>
 
-export default Navbar
+        <NavLink to="/contact" className={styles.button} activeClassName={styles.active}>
+          Contact
+        </NavLink>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
